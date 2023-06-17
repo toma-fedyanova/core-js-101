0c3ece10bcd6fe82c97e45c3f7bbcc36e7f7a565
Tamara Fedyanova <112993598+toma-fedyanova@users.noreply.github.com>
2023-06-17 16:58:02 +1000

    feat: add code

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -27,8 +27,12 @@
  *  21 => 'Fizz'
  *
  */
-function getFizzBuzz(/* num */) {
-  throw new Error('Not implemented');
+function getFizzBuzz(num) {
+  let value = num;
+  if (num % 15 === 0 && num % 3 === 0) value = 'FizzBuzz';
+  else if (num % 3 === 0) value = 'Fizz';
+  else if (num % 5 === 0) value = 'Buzz';
+  return value;
 }
 
 
@@ -43,8 +47,9 @@ function getFizzBuzz(/* num */) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
+function getFactorial(n) {
+  if (n === 1) return 1;
+  return n * getFactorial(n - 1);
 }
 
 
@@ -60,8 +65,15 @@ function getFactorial(/* n */) {
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers(/* n1, n2 */) {
-  throw new Error('Not implemented');
+function getSumBetweenNumbers(n1, n2) {
+  let start = n1;
+  const finish = n2;
+  let res = 0;
+  while (start <= finish) {
+    res += start;
+    start += 1;
+  }
+  return res;
 }
 
 
--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -186,8 +186,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let num = startFrom - 1;
+  return function func() {
+    num += 1;
+    return num;
+  };
 }
 
 
